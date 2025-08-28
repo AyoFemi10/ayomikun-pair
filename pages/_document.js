724de3d6aa097e22d4e1d4530d497d2dd02ce2eb
AyoFemi <emmanuelkelebe@gmail.com>
2025-08-28 03:52:56 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script src="https://js.paystack.co/v1/inline.js" async></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
